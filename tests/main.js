fcf4262526a3e7876e53678bd6948bea95c1cae4
lcomino <lcomino3d@gmail.com>
2015-07-20 22:13:26 -0300

    Adicionadas dependencias

--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,16 @@
 window.onload = function(){
 
-  Repeater.init('.repeater');
-  
+  var data = [{
+    "nome" : "Lucas",
+    "sobrenome" : "Comino",
+    "telefone" : "+55 44 9999-9999"
+    },
+    {
+      "nome" : "João",
+      "sobrenome" : "Silva",
+      "telefone" : "+55 44 8888-9999"
+  }];
+
+  Repeater.init('.repeater', data);
+
 };
