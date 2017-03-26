34203990ea5d541613f406c274ed8645174803d1
Lucas Comino <Lucas Comino>
2017-03-25 23:50:47 -0300

    reestruturadas pastas, dependencias inclusas no arquivo dist

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+let gulp = require('gulp');
+let uglify = require('gulp-uglify');
+let concat = require('gulp-concat');
+let Server = require('karma').Server;
+
+/**
+ * Run test once and exit
+ */
+gulp.task('test', function (done) {
+  new Server({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done).start();
+});
+
+gulp.task('default', function(){  
+  return gulp.src(['bower_components/StringJs/dist/string.js', 'bower_components/ObjectJs/object.js', 'src/*.js'])  
+  .pipe(concat('repeater.js'))
+  .pipe(uglify({
+      mangle: false
+    }))
+  .pipe(gulp.dest('dist'));
+});
